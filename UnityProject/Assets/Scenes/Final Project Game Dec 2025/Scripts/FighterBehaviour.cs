37446c9f897579c3dddc261cd8187a39f4eedea8
Quinn Johnston <65921679+buhfo@users.noreply.github.com>
2025-12-05 17:12:28 -0700

    Generic Fighter character AI started

---
+++ UnityProject/Assets/Scenes/Final Project Game Dec 2025/Scripts/FighterBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Collider))]
+public class FighterBehaviour : MonoBehaviour
+{
+    public FighterStats aiStatsObj;
+    private AiBase aiBaseObj;
+    private NavMeshAgent agent;
+    public UnityEvent startEvent, triggerEnterEvent, triggerExitEvent;
+    
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        startEvent.Invoke();
+        
+        if (aiStatsObj == null) return;
+        agent.speed = aiStatsObj.speed;
+        agent.acceleration = aiStatsObj.acceleration;
+        agent.angularSpeed = aiStatsObj.angularSpeed;
+        agent.stoppingDistance = aiStatsObj.stoppingDistance;
+    }
+
+    public void ChangeBase(AiBase obj)
+    {
+        aiBaseObj = obj;
+    }
+    private void Update()
+    {
+        aiBaseObj.Navigate(agent);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggerExitEvent.Invoke();
+    }
+}
---
+++ UnityProject/Assets/Scenes/Final Project Game Dec 2025/Scripts/FighterStats.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FighterStats", menuName = "AI/FighterStats")]
+public class FighterStats : ScriptableObject
+{
+    //NavMeshAgent settings
+    public float angularSpeed = 120f;
+    public float acceleration = 8f;
+    public float stoppingDistance = 0.5f;
+    
+    //Stats for AI characters
+    public float health = 100f;
+    public float speed = 3.5f;
+    public float stamina = 100f;
+    public float attackDamage = 10f;
+}
