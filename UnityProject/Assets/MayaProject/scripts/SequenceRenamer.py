03ff359aa86bdee9205c7fddb463f5e3e65aba72
Quinn Johnston <65921679+buhfo@users.noreply.github.com>
2025-12-08 14:28:33 -0700

    Sequence Renamer UI functional and seperate

--- UnityProject/Assets/MayaProject/scripts/SequenceRenamer.py
+++ UnityProject/Assets/MayaProject/scripts/SequenceRenamer.py
@@ -26,39 +26,4 @@ def rename(new_name_pattern):
         # Rename the object
         cmds.rename(obj, new_name)
 
-### UI Class###
-class SequenceRenamerUI():
-    def __init__(self):
-        pass
-    
-    WINDOW_NAME = "SequenceRenamerWindow"
 
-    @classmethod
-    def show(cls):
-        # Makes sure theres only one window
-        if cmds.window(cls.WINDOW_NAME, exists=True):
-            cmds.deleteUI(cls.WINDOW_NAME)
-
-        # Makes the window
-        cmds.window(cls.WINDOW_NAME, title="Sequence Renamer", widthHeight=(300, 100))
-        cmds.columnLayout(adjustableColumn=True)
-
-        # Add an input box to name the new pattern
-        cls.name_field = cmds.textFieldGrp(label="New Name Pattern:", text="L_Leg_##_Ctrl")
-
-        # adds the button to make it go 
-        cmds.button(label="Rename Selected Objects", command=cls.rename_selected_objects)
-
-        # Show the window
-        cmds.showWindow(cls.WINDOW_NAME)
-
-    @classmethod
-    def rename_selected_objects(cls, *args):
-        # Get the new name
-        new_name_pattern = cmds.textFieldGrp(cls.name_field, query=True, text=True)
-        
-        # Call the rename function
-        rename(new_name_pattern)
-
-
-SequenceRenamerUI.show()
