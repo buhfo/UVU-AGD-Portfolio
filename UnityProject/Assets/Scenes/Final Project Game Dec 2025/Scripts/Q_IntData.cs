4b6365cdffc3369f1e6604661e9c63501556d62d
Quinn Johnston <quinnandout@gmail.com>
2025-12-12 20:44:41 -0700

    Finished enemy AI, fixed some scripts, applied textures

---
+++ UnityProject/Assets/Scenes/Final Project Game Dec 2025/Scripts/Q_IntData.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/QIntData")]
+public class QIntData : ScriptableObject
+{
+    [SerializeField] private int value, minValue, maxValue;
+
+    public UnityEvent<float> valueOutOfRange;
+    public UnityEvent onValueChanged, onValueZero;
+
+    public int Value
+    {
+        get => value;
+        set
+        {
+            this.value = value;
+            onValueChanged.Invoke();
+            ClampValue();
+        }
+    }
+
+    public void UpdateValue(int amount)
+    {
+        value += amount;
+    }
+
+    public void SetValue(IntData data)
+    {
+        value = data.Value;
+    }
+    
+    public void SetValue(int data)
+    {
+        Value = data;
+    }
+    
+    public void IncrementValue()
+    {
+        value++;
+        onValueChanged.Invoke();
+    }
+
+    private void ClampValue()
+    {
+        if (!(Value < minValue) && !(Value > maxValue)) return;
+        valueOutOfRange.Invoke(Value);
+        Value = Mathf.Clamp(Value, minValue, maxValue);
+   
+        if (value == 0)
+        {
+            onValueZero.Invoke();
+        }
+    }
+
+    public void UpdateValueZeroCheck(int i)
+    {
+        if (value + i < 0) return;
+        value += i;
+    }
+
+    public void ValueIsZero()
+    {
+        if (value == 0)
+        {
+            onValueZero.Invoke();
+        }
+        
+    }
+}
---
+++ UnityProject/Assets/Scenes/Final Project Game Dec 2025/Scripts/Q_TriggerEventsBehaviour.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class QTriggerEventsBehaviour : MonoEventsBehaviour
+{
+    public UnityEvent triggerEnterEvent, triggerEnterRepeatEvent, triggerEnterEndEvent, triggerExitEvent;
+    private WaitForSeconds waitEnterObj, waitRepeatObj, waitExitObj;
+    public float enterHoldTime = 0.01f, repeatHoldTime = 0.01f, exitHoldTime = 0.01f;
+    public bool canRepeat, canRepeatWithLimits;
+    private bool repeat;
+    public int repeatLimit = 10;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        waitEnterObj = new WaitForSeconds(enterHoldTime);
+        waitRepeatObj = new WaitForSeconds(repeatHoldTime);
+        waitExitObj = new WaitForSeconds(exitHoldTime);
+    }
+
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        yield return waitEnterObj;
+        triggerEnterEvent.Invoke();
+        
+        if (canRepeatWithLimits)
+        {
+            var count = 0;
+            while (repeat && count < repeatLimit)
+            {
+                yield return waitRepeatObj;
+                triggerEnterRepeatEvent.Invoke();
+                count++;
+            }
+        }
+        
+        if (canRepeat)
+            repeat = true;
+            while (repeat)
+            {
+                yield return waitRepeatObj;
+                triggerEnterRepeatEvent.Invoke();
+            }
+        
+
+        yield return waitExitObj;
+        triggerEnterEndEvent.Invoke();
+    }
+    
+
+    private void OnTriggerExit(Collider other)
+    {
+        repeat = false;
+        triggerExitEvent.Invoke();
+    }
+}
