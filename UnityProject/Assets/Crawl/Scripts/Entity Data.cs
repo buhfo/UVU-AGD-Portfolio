eab614a0ed9f1b644a5bc64c8f6032ce74c0e592
Quinn Johnston <quinnandout@gmail.com>
2026-02-11 12:55:53 -0700

    Added SOs for movement, and entity stats

---
+++ UnityProject/Assets/Crawl/Scripts/Entity Data.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(menuName = "Crawl/ScriptableObjects/EntityData")]
+public class EntityData : ScriptableObject
+{
+    [SerializeField] private int Health, Experience, Attack;
+    // [SerializeField] private int value, minValue, maxValue;
+
+    // public UnityEvent<float> valueOutOfRange;
+    // public UnityEvent onValueChanged, onValueZero;
+
+    // public int Value
+    // {
+    //     get => value;
+    //     set
+    //     {
+    //         this.value = value;
+    //         onValueChanged.Invoke();
+    //         ClampValue();
+    //     }
+    // }
+
+    // public void UpdateValue(int amount)
+    // {
+    //     value += amount;
+    // }
+
+    // public void SetValue(EntityData data)
+    // {
+    //     value = data.value;
+    // }
+    
+    // public void SetValue(int data)
+    // {
+    //     Value = data;
+    // }
+    
+    // public void IncrementValue()
+    // {
+    //     value++;
+    //     onValueChanged.Invoke();
+    // }
+
+    // private void ClampValue()
+    // {
+    //     if (!(Value < minValue) && !(Value > maxValue)) return;
+    //     valueOutOfRange.Invoke(Value);
+    //     Value = Mathf.Clamp(Value, minValue, maxValue);
+   
+    //     if (value == 0)
+    //     {
+    //         onValueZero.Invoke();
+    //     }
+    // }
+
+    // public void UpdateValueZeroCheck(int i)
+    // {
+    //     if (value + i < 0) return;
+    //     value += i;
+    // }
+}
---
+++ UnityProject/Assets/Crawl/Scripts/GenericMovement.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(menuName = "Crawl/ScriptableObjects/MovementData")]
+public class MovementData : ScriptableObject
+{
+    [SerializeField] private float distance, speed, turnRadius, turnSpeed;
+}
