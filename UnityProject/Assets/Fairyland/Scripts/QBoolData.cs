bec6b12a83209624cd04942770dd7c2b5cede69c
buhfo <65921679+buhfo@users.noreply.github.com>
2026-06-15 14:55:08 -0600

    Fixed Broken Bool data

---
+++ UnityProject/Assets/Fairyland/Scripts/QBoolData.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Fairyland.Scripts
+{
+    [CreateAssetMenu(menuName = "ScriptableObjects/QBoolData")]
+    public class QBoolData : ScriptableObject
+    {
+        [SerializeField] private bool value;
+        public UnityEvent onValueChangeEvent, setTrueEvent, setFalseEvent;
+
+        public bool CurrentValue => value;
+
+        private bool Value
+        {
+            get => value;
+            set
+            {
+                if (this.value == value) return;
+                this.value = value;
+                onValueChangeEvent?.Invoke();
+            }
+        }
+
+        public void SetValue(bool valueChange)
+        {
+            Value = valueChange;
+            TriggerEventsBasedOnValue();
+        }
+
+        public void ToggleValue()
+        {
+            Value = !Value;
+            TriggerEventsBasedOnValue();
+        }
+
+        private void TriggerEventsBasedOnValue()
+        {
+            if (Value)
+            {
+                setTrueEvent?.Invoke();
+            }
+            else
+            {
+                setFalseEvent?.Invoke();
+            }
+        }
+    }
+}
