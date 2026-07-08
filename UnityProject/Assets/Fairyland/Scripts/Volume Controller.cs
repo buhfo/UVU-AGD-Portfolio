c742d5503a1806f1904cf76068c48fa321ee4f0f
Quinn Johnston <65921679+buhfo@users.noreply.github.com>
2026-07-08 13:01:45 -0600

    Volume controls now fully set up and properly working

--- UnityProject/Assets/Fairyland/Scripts/Volume Controller.cs
+++ UnityProject/Assets/Fairyland/Scripts/Volume Controller.cs
@@ -1,22 +1,46 @@
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 namespace Fairyland.Scripts
 {
     public class VolumeController : MonoBehaviour
     {
-        public AudioSource audioSource;
+        public Slider slider;
         public FloatDataQ volume;
-        public FloatDataQ masterVolume;
+        public AudioMixer myMixer;
+        public string myAudioMixerGroup;
+        
+        public UnityEvent muteEvent, unmuteEvent;
 
         private void Start()
         {
+            slider.value = volume.value;
             SetVolume();
         }
 
         public void SetVolume()
         {
-            var actualVol = (volume.value * masterVolume.value);
-            audioSource.volume = actualVol;
+            
+            if (myMixer == null)
+            {
+                Debug.LogError($"VolumeController on '{gameObject.name}' in scene '{gameObject.scene.name}' has no mixer assigned.", this);
+                return;
+            }
+
+            var volumeDb = volume.value;
+            
+            myMixer.SetFloat(myAudioMixerGroup, volumeDb);
+            
+            if (Mathf.Approximately(volumeDb, volume.minValue))
+            {
+                muteEvent.Invoke();
+            }
+            else
+            {
+                unmuteEvent.Invoke();
+            }
         }
     }
 }
